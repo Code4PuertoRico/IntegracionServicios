e693667a8f7331955fdd7906d9193905eabc1436
Yelitza Valladares <yvalladares@evertecinc.com>
2013-06-06 17:43:20 -0400

    Evertec

---
+++ WcfService1/WcfService1/IService1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        BeneficiarioX GetBeneficiario(string ssn1, string ssn2, string ssn3);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class BeneficiarioX
+    {
+        bool Activo = true;
+        string SSN1 = "000";
+        string SSN2 = "00";
+        string SSN3 = "0000";
+
+        string Nombre = "";
+        string SegundoNombre = "";
+        string ApellidoPaterno = "";
+        string ApellidoMaterno = "";
+        char? Sexo;
+        DateTime? FechaDeNacimiento;
+
+        int MunicipioRecidencia = 0;
+
+        List<ServiciosA> Servicios = new List<ServiciosA>();
+
+        [DataMember]
+        public bool activo
+        {
+            get { return Activo; }
+            set { Activo = value; }
+        }
+
+        //[DataMember]
+        //public string ssn
+        //{
+        //    get { return string.Format("{0}-{1}-{2}", SSN1, SSN2, SSN3); }
+        //    //set { stringValue = value; }
+        //}
+        [DataMember]
+        public string nombre
+        {
+            get { return Nombre; }
+            set { Nombre = value; }
+        }
+        [DataMember]
+        public string apellidoPaterno
+        {
+            get { return ApellidoPaterno; }
+            set { ApellidoPaterno = value; }
+        }
+
+        [DataMember]
+        public string apellidoMaterno
+        {
+            get { return ApellidoMaterno; }
+            set { ApellidoMaterno = value; }
+        }
+
+        [DataMember]
+        public int municipioRecidencia
+        {
+            get { return MunicipioRecidencia; }
+            set { MunicipioRecidencia = value; }
+        }
+
+        [DataMember]
+        public string segundoNombre
+        {
+            get { return SegundoNombre; }
+            set { SegundoNombre = value; }
+        }
+
+        [DataMember]
+        public char? sexo
+        {
+            get { return Sexo; }
+            set { Sexo = value; }
+        }
+
+        [DataMember]
+        public DateTime? fechaDeNacimiento
+        {
+            get { return FechaDeNacimiento; }
+            set { FechaDeNacimiento = value; }
+        }
+
+        [DataMember]
+        public List<ServiciosA> servicios
+        {
+            get { return Servicios; }
+            set { Servicios = value; }
+        }
+    }
+
+    [DataContract]
+    public class ServiciosA
+    {
+        string Servicio;
+        string CodigoDeServicio;
+        bool? ServicioActivo;
+        DateTime? FechaDeServicio;
+        bool? BeneficioActivo;
+
+        [DataMember]
+        public string servicio
+        {
+            get { return Servicio; }
+            set { Servicio = value; }
+        }
+
+        [DataMember]
+        public string codigoDeServicio
+        {
+            get { return CodigoDeServicio; }
+            set { CodigoDeServicio = value; }
+        }
+
+        [DataMember]
+        public bool? servicioActivo
+        {
+            get { return ServicioActivo; }
+            set { ServicioActivo = value; }
+        }
+
+        [DataMember]
+        public bool? beneficioActivo
+        {
+            get { return BeneficioActivo; }
+            set { BeneficioActivo = value; }
+        }
+
+        [DataMember]
+        public DateTime? fechaDeServicio
+        {
+            get { return FechaDeServicio; }
+            set { FechaDeServicio = value; }
+        }
+    }
+
+}
---
+++ WcfService1/WcfService1/Service1.svc.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        //public string GetData(int value)
+        //{
+        //    return string.Format("You entered: {0}", value);
+        //}
+
+        public BeneficiarioX GetBeneficiario(string ssn1, string ssn2, string ssn3)
+        {
+            if (ssn1.Length != 3 || ssn2.Length != 2 || ssn3.Length != 4)
+            {
+                throw new ArgumentNullException("Invalid SSN");
+            }
+           
+            DBConnectionDataContext DB = new DBConnectionDataContext();
+            var BEs = from e in DB.Beneficiarios where e.SSN1 == ssn1 && e.SSN2 == ssn2 && e.SSN3 == ssn3 select e;
+            
+            if (BEs.ToList().Count > 0)
+            {
+                var BE = BEs.First();
+                var SEs = from s in DB.Serv_Benefs where s.id_key == BE.id_Key select s;
+                BeneficiarioX B = new BeneficiarioX();
+                B.nombre = BE.Nombre;
+                B.segundoNombre = BE.SegundoNombre;
+                B.apellidoPaterno = BE.ApellidoPaterno;
+                B.apellidoMaterno = BE.ApellidoMaterno;
+                B.municipioRecidencia = BE.MunicipioResidencia;
+                B.sexo = BE.Sexo[0];
+                B.fechaDeNacimiento = BE.FechaDeNac;
+                foreach(Serv_Benef Se in SEs)
+                {
+                    ServiciosA SA = new ServiciosA();
+                    SA.servicio = Se.Servicio.Servicio1;
+                    SA.codigoDeServicio = Se.Servicio.CodServ;
+                    SA.fechaDeServicio = Se.FechaServ;
+                    SA.servicioActivo = Se.Activo;
+                    SA.beneficioActivo = Se.Servicio.Activo;
+                    B.servicios.Add(SA);
+                }
+                return B;
+            }
+            return null;
+        }
+    }
+}
